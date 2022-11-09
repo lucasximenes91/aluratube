74d6dce3d74ed6349ef0d4c4f4d931a5ec686830
lucasximenes91 <lucas_vovo@hotmail.com>
2022-11-08 23:31:13 -0300

    ajuste banner

--- pages/index.js
+++ pages/index.js
@@ -60,6 +60,8 @@ const StyledHeader = styled.div`
 `;
 const StyledBanner = styled.div`
 background-image: url(${config.bg});
+background-repeat: no-repeat; 
+background-size: cover;
 height: 230px;
 `
 function Header() {
